5bf156def96121324183b072f3b0e94fa5e76918
JimPeterle <179974209+JimPeterle@users.noreply.github.com>
2026-05-24 12:14:10 +0200

    Add installation of python3-cryptography package

--- setup.sh
+++ setup.sh
@@ -213,6 +213,7 @@ sudo apt install python3-qrcode -y
 sudo apt install python3-pil -y
 sudo apt install python3-apscheduler -y
 sudo apt install python3-sqlalchemy -y
+sudo apt install python3-cryptography -y
 
 
 echo "${YELLOW}Finished installing${NC}"
